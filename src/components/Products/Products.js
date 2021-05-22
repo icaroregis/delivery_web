2bc7eac3d10925b994c0e3a822507cc44afd01d4
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-05-22 17:27:50 -0300

    refatorando

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,60 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Col, Row } from 'react-materialize';
 import * as services from '../../services/deliveryServices';
 
-class Products extends React.Component {
-  //construtor..
-  constructor(props) {
-    super(props);
+export default function Products() {
+  const [cardapio, setCardapio] = useState([]);
 
-    //declarando o state do componente..
-    this.state = {
-      cardapio: [], //array vazio
-    };
-  }
-
-  //método executado antes do componente ser renderizado
-  componentDidMount() {
-    //acessando o serviço da API..
+  useEffect(() => {
     services
       .getCardapio()
       .then((data) => {
         //armazenar o retorno da API no state..
-        this.setState({ cardapio: data });
+        setCardapio(data);
       })
       .catch((e) => {
         console.log(e);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <Card>
-        {this.state.cardapio.map(function (item, i) {
-          return (
-            <Row key={i}>
-              <Col m={1}>
-                <img
-                  src={services.getApiUrl() + item.foto}
-                  className="responsive-img"
-                />
-              </Col>
-              <Col m={9}>
-                <h6>
-                  <strong>{item.nome}</strong>
-                </h6>
-                <p>{item.descricao}</p>
-              </Col>
-              <Col m={2}>
-                <h5>{item.preco}</h5>
-                <small>{item.categoria.nome}</small>
-              </Col>
-            </Row>
-          );
-        })}
-      </Card>
-    );
-  }
+  return (
+    <Card>
+      {cardapio.map((item, i) => {
+        return (
+          <Row key={i}>
+            <Col m={1}>
+              <img
+                src={services.getApiUrl() + item.foto}
+                className="responsive-img"
+              />
+            </Col>
+            <Col m={9}>
+              <h6>
+                <strong>{item.nome}</strong>
+              </h6>
+              <p>{item.descricao}</p>
+            </Col>
+            <Col m={2}>
+              <h5>{item.preco}</h5>
+              <small>{item.categoria.nome}</small>
+            </Col>
+          </Row>
+        );
+      })}
+    </Card>
+  );
 }
-
-export default Products;
