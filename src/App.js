c09c94a6f4161c3f640461cdc18a459e1e0c4ed1
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-05-29 09:46:12 -0300

    refatoração

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
-
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Main from './views/Main/Main';
 
 import { Row, Col } from 'react-materialize';
 
-class App extends React.Component {
-  render() {
-    return (
+export default function App() {
+  return (
+    <Row>
+      <Header />
       <Row>
-        <Header />
-        <Row>
-          <Col m={2}>
-            <Profile />
-          </Col>
-          <Col m={10}>
-            <Main />
-          </Col>
-        </Row>
+        <Col m={2}>
+          <Profile />
+        </Col>
+        <Col m={10}>
+          <Main />
+        </Col>
       </Row>
-    );
-  }
+    </Row>
+  );
 }
-
-export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { Row, Navbar } from 'react-materialize';
 import { NavLink } from 'react-router-dom';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <Row>
-        <Navbar className="deep-orange darken-2">
-          <li>
-            <NavLink to="/">Página inicial</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cadastrar_cliente">Crie sua conta de cliente</NavLink>
-          </li>
-        </Navbar>
-      </Row>
-    );
-  }
+export default function Header() {
+  return (
+    <Row>
+      <Navbar className="deep-orange darken-2">
+        <li>
+          <NavLink to="/">Página inicial</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cadastrar_cliente">Crie sua conta de cliente</NavLink>
+        </li>
+      </Navbar>
+    </Row>
+  );
 }
-
-export default Header;
