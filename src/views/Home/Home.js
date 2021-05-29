006f69bd0400019486845811a531d66b51f0f33f
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-05-29 09:49:04 -0300

    refatoração

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Products from '../../components/Products/Products';
 
-const Home = () => <Products />;
-
-export default Home;
+export default function Home() {
+  return <Products />;
+}
